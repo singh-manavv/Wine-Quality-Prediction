cf8dcbac49da973074dc4567ba1f7b2e262aca8a
Manvendra Singh <MANVENDRAS2608@GMAIL.COM>
2024-02-27 04:12:46 +0530

    Requirements added

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = 'wine-quality'
+project_name = 'wineQuality'
 
 list_of_files = [
     
