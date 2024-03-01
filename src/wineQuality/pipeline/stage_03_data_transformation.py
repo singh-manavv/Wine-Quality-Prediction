688bc294c1553bd806b063a985c64ba60375489d
Manvendra Singh <MANVENDRAS2608@GMAIL.COM>
2024-03-01 16:17:25 +0530

    Data Transformation Updated

--- src/wineQuality/pipeline/stage_03_data_transformation.py
+++ src/wineQuality/pipeline/stage_03_data_transformation.py
@@ -1,7 +1,7 @@
 from wineQuality.config import ConfigurationManager
 from wineQuality.components import DataTransformation
 from wineQuality import logger
-
+from pathlib import Path
 STAGE_NAME = 'Data Transformation'
 
 class DataTransformationTrainPipeline:
@@ -9,10 +9,20 @@ class DataTransformationTrainPipeline:
         pass
     
     def main(self):
-        config = ConfigurationManager()
-        data_transformation_config = config.get_data_transformation_config()
-        data_transformation = DataTransformation(config=data_transformation_config)
-        data_transformation.splitting_data()
+        try:
+            with open(Path('artifacts/data_validation/status.txt'), 'r') as f:
+                status = f.read().split(' ')[-1]
+            if status == 'True':
+                config = ConfigurationManager()
+                data_transformation_config = config.get_data_transformation_config()
+                data_transformation = DataTransformation(config=data_transformation_config)
+                data_transformation.splitting_data()
+            else:
+                logger.info(f'Invalid Data schema')
+                raise Exception('Invalid Data schema')
+        except Exception as e:
+            raise e
+        
 
 if __name__ == '__main__':
     try:
